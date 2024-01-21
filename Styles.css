389eb23af4407f8e331c3cb43e814799f185322d
Wubkitty <157272997+Wubkitty@users.noreply.github.com>
2024-01-21 16:00:07 +0800

    Add files via upload

---
+++ Styles.css
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wubwub</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="wrapper">
+        <h2 class="question">Will you go to the concert with me wubwub?</h2>
+        <div class="gif">
+            <iframe src="https://giphy.com/embed/qlAPmI5m92Lo1cial8" width="480" height="405" frameborder="0" class="giphy-embed" allowfullscreen></iframe>
+            <p><a href="https://giphy.com/stickers/achtung-tw-transparent-qlAPmI5m92Lo1cial8">via GIPHY</a></p>
+        </div>
+        <div class="btn-group">
+            <button class="yes-btn">Yes</button>
+            <button class="no-btn">No</button>
+        </div>
+        <div class="confirmation-message">
+            <h2>Yay! See youuu blee I love you mwa</h2>
+            <!-- Additional content for confirmation message -->
+        </div>
+        <audio controls autoplay loop>
+            <source src="https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-14867/zapsplat_multimedia_click_003_19369.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
